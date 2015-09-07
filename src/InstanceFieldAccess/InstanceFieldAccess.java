ae091dc55738072a5c5ff3af2e5997a61baa7a3c
XiaoQiao <shuxiaoqiao@cldmind.com>
2015-09-07 16:44:00 +0800

    1. add package HelloWorld 2. add package InstanceFieldAccess

---
+++ src/InstanceFieldAccess/InstanceFieldAccess.java
@@ -0,0 +1,13 @@
+package InstanceFieldAccess;
+
+public class InstanceFieldAccess {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/run.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+export LD_LIBRARY_PATH=./HelloWorld
+java HelloWorld.HelloWorld
